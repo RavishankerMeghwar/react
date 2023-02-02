181bd4247a85e73179de4c4afba8433a3abb2d4a
RavishankerMeghwar <ravishankermeghwar@gmail.com>
2023-02-03 00:53:16 +0500

    first commit

---
+++ crud/src/components/SignUpForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // submit form data to the server
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        User Name:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleInputChange}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignUpForm;
